8e45e464f106f5b84b4a6643bc68cccc487efce0
Francisco Jose <francisco_jose13@hotmail.es>
2020-05-16 11:00:15 +0200

    Modificado 1

--- Kata1/1.py
+++ Kata1/1.py
@@ -1,3 +1,13 @@
 '''
-Instalar y poner a punto
+1.- Instalar Python 3.8
+2.- Instalar Git 2.25
+3.- Instalar Visual Studio Code
+4.- Crear una Carpeta para las Katas ( Hackathon )
+5.- Abrirlo con VSC ( File -> Open Folder )
+6.- Crear Repositorio de Git (git init o 3º Icono del VSC con boton)
+7.- git config --global user.name "Nombre"
+8.- git config --global user.email "name@correo.com"
+9.- Crear Carpetas de Kata 1 al 4 con main.Python
+10.- Hacer un Commit
+Extra.- git log (para ver los commit realizados)
 '''
